d5ab27ffac7ea43ee6d7177b855015b20377aebb
raykhard <61735357+raykhard@users.noreply.github.com>
2020-05-16 21:00:13 +0300

    Update inventController.js

--- controllers/inventController.js
+++ controllers/inventController.js
@@ -1,29 +1,6 @@
 const inventory = require("../models/inventory");
 
 module.exports = {
-    //add new stock to the Inventory
-     AddStock: (req, res, ) => {
-         const NewStock = new inventory({
-            categoryId: req.body.categoryId,
-            name: req.body.name,
-            description: req.body.description,
-            category: req.body.category,
-            price: req.body.price,
-            number_in_stock: req.body.number_in_stock
-        });
-        NewStock.save()
-        .then((saved) => {
-            res.status(200).json({
-                message: 'you have added an item',
-                NewStock: NewStock
-
-            })
-            console.log('new stock saved', saved);
-        })
-        .catch((error)=>{
-            console.log('error', error);
-        });
-    },
     putStock:(req, res)=>{
         //update already existing stock
         inventory.updateOne({
