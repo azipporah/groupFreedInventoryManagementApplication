d965d68ab1dec1ddf37f4e7d3c6ed1aff6e539d5
raykhard <61735357+raykhard@users.noreply.github.com>
2020-05-16 20:57:05 +0300

    Update router.js

--- routes/router.js
+++ routes/router.js
@@ -1,7 +1,6 @@
 const{ AddStock, deleteStock, putStock} = require("../controllers/inventController");
 const router = require("express-promise-router")();
 
-router.route("/inventory").post(AddStock);
 router.route("/inventory").delete(deleteStock);
 router.route("/inventory").put(putStock)
 
