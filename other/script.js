e981fe8b86aa17c7351fef610b02694ab3797321
azipporah <zipporahatusasiire@gmail.com>
2020-05-27 12:19:46 +0200

    removing from master

---
+++ other/script.js
@@ -0,0 +1,31 @@
+require('../models/mongodb');
+ 
+//Import the necessary packages
+const express = require('express');
+var app = express();
+const path = require('path');
+const exphb = require('express-handlebars');
+const bodyparser = require('body-parser');
+ 
+const itemController = require('./controller/itemController');
+ 
+app.use(bodyparser.urlencoded({
+extended: true
+}));
+ 
+
+app.get('/', (req, res) => {
+res.send
+});
+app.use(bodyparser.json());
+ 
+//Establish the server connection
+// const port = process.env.PORT || 6000;
+// app.listen(port, () => console.log(`Listening on port ${port}`));
+
+app.listen(3000, function(){
+    console.log('server started at port 3000');    
+});
+ 
+//Set the Controller path which will be responding to the user actions
+app.use('/item', itemController);
