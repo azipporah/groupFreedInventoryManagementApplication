9d5103049ea03aa9113275c5837affb405ca34ba
azipporah <zipporahatusasiire@gmail.com>
2020-05-27 12:24:08 +0200

    removing from master

--- models/Category.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require("mongoose");
-const categorySchema = require("./schemas/categorySchema");
-
-const Category = mongoose.model("inventories", categorySchema);
-//inventories is the name of the collection in the database
-
-module.exports = Category;
