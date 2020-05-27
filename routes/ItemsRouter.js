6ff9cbc2249c903973565233f06269b68fef9bb6
azipporah <zipporahatusasiire@gmail.com>
2020-05-27 23:40:38 +0200

    merge conflict

--- routes/ItemsRouter.js
+++
@@ -1,9 +0,0 @@
-//using the express promise router
-const router = require('express-promise-router')();
-const {GetInventory, GetCategory, GetCategoryId} = require('../controllers/itemController');
-
-router.route("/:inventory").get(GetInventory);
-router.route('/inventory/category').get(GetCategory);
-router.route('/inventory/:categoryId').get(GetCategoryId);
-
-module.exports = router
