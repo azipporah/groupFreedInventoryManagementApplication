b1eeaf93c4a26fa8e006e483ef4c128c6653985f
azipporah <zipporahatusasiire@gmail.com>
2020-05-28 16:29:16 +0200

    merge conflict

--- app.js
+++ app.js
@@ -4,10 +4,6 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const router = require("./routes/router");
-const UsersRouter = require('./routes/UsersRouter');
-const putRouter = require("./routes/router");
-const ItemsRouter = require('./routes/ItemsRouter');
 
 //to keep important info from public
 require('dotenv/config');
@@ -20,10 +16,6 @@ const authRoute = require('./routes/auth');
 app.use(bodyParser.json());
 app.use(cors());
 app.use('/', authRoute);//middleware for the router
-app.use(UsersRouter);
-app.use(putRouter);
-app.use(ItemsRouter);
-app.use(router);
 
 //to connect to the server
 app.listen(4550, function(){
